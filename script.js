33dfcf73681a0d977f628acfb97120ac3913d9eb
Udhav Sharma <96074679+UdhavSharma2004@users.noreply.github.com>
2022-08-30 22:54:52 +0530

    UI changed

--- script.js
+++ script.js
@@ -1,6 +1,65 @@
-let headobj=prompt("Welcome to Task Organizer prepared by Udhav Sharma, Thanks for choosing this application.\n\nCan you please share your name as user.")
-if(headobj==null||headobj==""){
-    headobj="Unknown"
+let today=new Date();
+let dateobj=document.getElementById("htime");
+let dayvalue,monthvalue;
+switch(today.getUTCDay()){
+case 0:
+    dayvalue="Sunday"
+    break;
+case 1:
+    dayvalue="Monday"
+    break;
+case 2:
+    dayvalue="Tuesday"
+    break;
+case 3:
+    dayvalue="Wednesday"
+    break;
+case 4:
+    dayvalue="Thrusday"
+    break;
+case 5:
+    dayvalue="Friday"
+    break;
+case 6:
+    dayvalue="Saturday"
+    break;
 }
-document.getElementById("headingid").innerText+=headobj;
-document.getElementsByClassName("checkbox1")
+switch(today.getUTCMonth()){
+    case 0:
+        monthvalue="January"
+        break;
+    case 1:
+        monthvalue="Feburary"
+        break;
+    case 2:
+        monthvalue="March"
+        break;
+    case 3:
+        monthvalue="April"
+        break;
+    case 4:
+        monthvalue="May"
+        break;
+    case 5:
+        monthvalue="June"
+        break;
+    case 6:
+        monthvalue="July"
+        break;
+    case 7:
+        monthvalue="August"
+        break;
+    case 8:
+        monthvalue="September"
+        break;
+    case 9:
+        monthvalue="October"
+        break;
+    case 10:
+        monthvalue="November"
+        break;
+    case 11:
+        monthvalue="December"
+        break;
+}
+dateobj.innerText=dayvalue+", "+(today.getUTCDate()+1)+" "+monthvalue+" "+today.getUTCFullYear();
